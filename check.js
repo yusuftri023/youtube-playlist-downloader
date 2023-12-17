70339963afc756805fbdddcc5f4e3f29caa136af
yusuftri023 <yusuftri023@gmail.com>
2023-12-17 12:36:41 +0700

    fix: .env files and variable

--- check.js
+++ check.js
@@ -1,11 +1,13 @@
 const { google } = require("googleapis");
 const youtubeAPI = google.youtube("v3");
+require("dotenv").config();
+const { GOOGLE_API } = process.env;
 function playlistDownload(playlistId) {
   youtubeAPI.playlistItems.list(
     {
-      key: "AIzaSyBtAu7Xvt_JaN5xa7jPcXcLuNL0g6IZelQ",
+      key: GOOGLE_API,
       part: "id,snippet",
-      playlistId: "PLC3y8-rFHvwjPxNAKvZpdnsr41E0fCMMP",
+      playlistId: playlistId,
       pageToken: "",
       maxResults: 50,
     },
